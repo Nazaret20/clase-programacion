ebfd611a4f57496e09b9ca1593cce8f3bc0676d5
Nazaret <nazaretgutirrezmartn@gmail.com>
2025-11-05 10:52:46 +0100

    Ejercicios java

--- PiramideNums.java
+++ PiramideNums.java
@@ -2,14 +2,13 @@ import java.util.Scanner;
 
 public class PiramideNums {
     public static void main(String[] args) {
-        int alto, espaciosLat, contador;
+        int alto, espaciosLat;
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Dime el alto: ");
         alto = sc.nextInt();
 
         espaciosLat = alto - 1;
-        contador = 1;
 
         for (int linea = 1; linea <= alto; linea++) {
             for (int i = 1; i <= espaciosLat; i++) {
