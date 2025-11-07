dc767682bb195ee24bcc18a576e8bd32dd05a0da
Nazaret <nazaretgutirrezmartn@gmail.com>
2025-11-07 12:02:16 +0100

    ejercicio java

--- NumRomanos.java
+++ NumRomanos.java
@@ -1,9 +1,82 @@
 public class NumRomanos {
     public static void main(String[] args) {
-        int I = 1, v = 5, x = 10, l = 50, c = 100, d = 500, m = 1000, num;
-        num = 1124;
+        int num;
 
-    
+        num = 1998;
 
+        // if (num >= 3000) {
+        //     System.out.print("MMM");
+
+        // } else if (num >= 2000) {
+        //     System.out.print("MM");
+
+        // } else if (num >= 1000) {
+        //     System.out.print("M");
+        // }
+
+        while (num >= 1000) {
+            System.out.print("M");
+            num -= 1000;
+        }
+
+        if (num >= 900) {
+            System.out.print("CM");
+            num -= 900;
+        }
+
+        if (num >= 500) {
+            System.out.print("D");
+            num -= 500;
+        }
+
+        if (num >= 400) {
+            System.err.print("CD");
+            num -= 400;
+        }
+
+        while (num >= 100) {
+            System.out.print("C");
+            num -= 100;
+        }
+
+        if (num >= 90) {
+            System.out.print("XC");
+            num -= 90;
+        }
+
+        if (num >= 50) {
+            System.out.print("L");
+            num -= 50;
+        }
+
+        if (num >= 40) {
+            System.err.print("XL");
+            num -= 40;
+        }
+
+        while (num >= 10) {
+            System.out.print("X");
+            num -= 10;
+        }
+
+        if (num >= 9) {
+            System.out.print("IX");
+            num -= 9;
+        }
+
+        if (num >= 5) {
+            System.out.print("V");
+            num -= 5;
+        }
+
+        if (num >= 4) {
+            System.err.print("IX");
+            num -= 4;
+        }
+
+        while (num >= 1) {
+            System.out.print("I");
+            num -= 1;
+        }
     }
 }
